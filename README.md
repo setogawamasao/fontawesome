d8f4a371e262e01a3b10b5c9ec2a03000616749f
masaosetogawa <ms.job.hunting@gmail.com>
2020-05-17 18:00:44 +0900

    Add fontawesome

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-## XXXXXXXXXXXXXXXXXX
+## 参考サイト
 
-![DEMO](./gif/demo.gif)
+https://qiita.com/mecaota/items/4483481a67a9b2cab37f
+https://qiita.com/riversun/items/4faa56ac40071f638313#2-1%E5%9F%BA%E6%9C%AC%E3%83%91%E3%83%83%E3%82%B1%E3%83%BC%E3%82%B8%E7%89%88-font-awesome-javascript-%E3%81%AE%E3%82%A4%E3%83%B3%E3%82%B9%E3%83%88%E3%83%BC%E3%83%AB%E3%81%A8%E5%AE%9F%E8%A1%8C
+
+### インストール
+
+npm i --save @fortawesome/react-fontawesome
+npm i --save @fortawesome/fontawesome-svg-core
+npm i --save @fortawesome/free-solid-svg-icons
+npm i --save @fortawesome/free-brands-svg-icons
+npm i --save @fortawesome/free-regular-svg-icons
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core"; //fontawesomeのコアファイル
+import { fab } from "@fortawesome/free-brands-svg-icons"; //fontawesomeのbrandアイコンのインポート
+import { fas } from "@fortawesome/free-solid-svg-icons"; //fontawesomeのsolidアイコンのインポート
+import { far } from "@fortawesome/free-regular-svg-icons"; //fontawesomeのregularアイコンのインポート
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-class App extends React.Component {
-  render() {
-    return <h1>HELLO REACT + TYPESCRIPT WORLD</h1>;
-  }
-}
-
-//const App = () => {
-//  return <h1>HELLO REACT + TYPESCRIPT WORLD</h1>
-//};
+const App = () => {
+  library.add(fab, fas, far); //他のコンポーネントから簡単に呼び出せるようにするための登録処理？
+  return (
+    <>
+      <FontAwesomeIcon icon={["fas", "minus-circle"]} />
+      <FontAwesomeIcon icon={["fab", "github"]} />
+    </>
+  );
+};
 
 export default App;
